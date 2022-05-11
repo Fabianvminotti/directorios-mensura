2f41df977c69676b25c25950ecbdcf16b99aa328
Fabian Minotti <fabianvminotti@gmail.com>
2022-05-11 16:23:55 -0300

    mejoras en creacion de carpetas

--- prueba2.py
+++ prueba2.py
@@ -3,18 +3,37 @@ import csv
 from datetime import datetime
 
 
+#########FUNCIONES#######################
+def CreacionCarpetas():
+        conf= input('desea crear %i carpetas? (SI/NO)' %i)
+        print(conf)
+        if conf == 'SI':
+                for fila in f:
+                        if os.path.exists(ruta + '/' + fila.replace('\n','') ):    
+                                print('La ruta '+ ruta + '/' + fila.replace('\n','') +' Ya existe')
+                        else:
+                                try:
+                                        os.mkdir(ruta + '/' + fila.replace('\n','') )
+                                except:
+                                        print("Ocurrio un error en la creacion de la carpeta" + fila.replace('\n','') )
+                                else:
+                                        os.mkdir(ruta + '/' + fila.replace('\n','') )
+        elif conf == 'NO':
+                print('programa finalizado')
+        else:
+                print('Ingresar un SI o NO')
+                CreacionCarpetas()
+
+#######################CODIGO########################                
+
 ruta = os.getcwd()
-
-
 f = open("csvprueba.csv", "r")
 
+#rutaUser = input('Insertar ruta del directorio donde crear las carpetas')
 
-ruta = os.getcwd()
+#########Contador de filas del csv##########
+i=0
+for entrada in f:
+        i=i+1
 
-for fila in f:
-    nombreCarpeta =  fila.replace('\n','')
-    if os.path.exists(ruta + '/' + nombreCarpeta ):    
-            print('La ruta '+ ruta + '/' + nombreCarpeta +' Ya existe')
-    else:
-            os.mkdir(ruta + '/' + nombreCarpeta )
-
+CreacionCarpetas()
